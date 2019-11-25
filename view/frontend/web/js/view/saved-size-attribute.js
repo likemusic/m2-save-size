70a37afe03c18191b64168c2cad3481ccb7d6e12
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-11-25 12:19:00 +0300

    Implement call SwatchRendered->_EmulateSelected() with private section data.

--- view/frontend/web/js/view/saved-size-attribute.js
+++ view/frontend/web/js/view/saved-size-attribute.js
@@ -1,14 +1,45 @@
 define([
-    'uiComponent',
+    'jquery',
     'Magento_Customer/js/customer-data'
-], function (Component, customerData) {
+], function ($, customerData) {
     'use strict';
 
+    function onSectionLoaded(sections) {
+        var section = sections['saved-size-attribute'];
+
+        var attributeCode = section['attribute_code'];
+        var valueId = section['value_id'];
+
+        var selectedAttributes = {};
+        selectedAttributes[attributeCode] = valueId;
+
+        setSwatchersAttributes(selectedAttributes);
+    }
+
+    function setSwatchersAttributes(selectedAttributes) {
+        var renderers = $(':mage-SwatchRenderer');
+        renderers.each(function (index, renderer) {
+            var a = 1;
+            // $(renderer).SwatchRenderer('_EmulateSelected', selectedAttributes);
+            var control = $(renderer).data('mage-SwatchRenderer');
+            control._EmulateSelected(selectedAttributes);
+        })
+    }
+
+    return function() {
+        customerData.reload(['saved-size-attribute']).done(onSectionLoaded);
+    };
+
+    // customerData.reload(['saved-size-attribute']).done(onSectionLoaded);
+
+/*
     return Component.extend({
         initialize: function () {
             this._super();
-            customerData.reload(['saved-size-attribute']);
+            customerData.reload(['saved-size-attribute']).done(onSectionLoaded);
+            // customerData.reload(['saved-size-attribute']);
             this['savedSizeAttribute'] = customerData.get('saved-size-attribute');
         }
     });
+*/
 });
